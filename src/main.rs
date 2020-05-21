b2459b3711d418bc064185a7c26ccc2b093be27e
Selçuk Arslan <me@sarslan.net>
2020-05-21 00:21:55 -0400

    sample is added.

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,19 @@ use opencv::{
 
 fn get_faces(frame: &opencv::prelude::Mat) -> opencv::Result<types::VectorOfRect> {
     let mut gray = Mat::default()?;
-    imgproc::cvt_color(frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+    imgproc::cvt_color(&frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
 
     let xml= core::find_file("haarcascades/haarcascade_frontalface_default.xml", true, false)?;
     let mut face = objdetect::CascadeClassifier::new(&xml)?;
 
     let mut faces = types::VectorOfRect::new();
-    face.detect_multi_scale(&gray, &mut faces, 1.1, 3, 0, core::Size{width: 40, height: 40 }, core::Size { width: 0, height: 0 })?;
+    face.detect_multi_scale(&gray, &mut faces, 1.1, 3, 0, core::Size{width: 300, height: 300 }, core::Size { width: 0, height: 0 })?;
 
     return Ok(faces);
 }
 
 fn run() -> opencv::Result<()> { 
-    let mut frame = opencv::imgcodecs::imread("input-2.jpg", opencv::imgcodecs::IMREAD_COLOR)?;
+    let mut frame = opencv::imgcodecs::imread("./data/input.jpg", opencv::imgcodecs::IMREAD_COLOR)?;
 
     let faces = get_faces(&mut frame)?;
 
@@ -35,7 +35,7 @@ fn run() -> opencv::Result<()> {
     }
 
     let mut params: core::Vector<i32> = core::Vector::new();
-    opencv::imgcodecs::imwrite("output-2.jpg", &frame, &mut params)?;
+    opencv::imgcodecs::imwrite("./data/output.jpg", &frame, &mut params)?;
 
     return Ok(())
 }
